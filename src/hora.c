cf0df51a474147f28949bacca87ae4da70118665
AndresLopez420 <73365005+AndresLopez420@users.noreply.github.com>
2020-10-23 17:53:19 -0300

    Update hora.c
    cambio while true por while 1

--- src/hora.c
+++ src/hora.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-/* Se importan las bibliotecas*/
+/* Se importan las librerias*/
  
 void senales(int signum){ // funcion sin retorno llamada senales con el parametro signum
     if (signum == SIGUSR1){//condicion que verifica que la señal corresponda al proceso
@@ -16,7 +16,7 @@ void senales(int signum){ // funcion sin retorno llamada senales con el parametr
 }
 
 int main(){// funcion principal
-	while(True){//bucle infinito
+	while(1){//bucle infinito
 		printf("Listo para recibir la señal PID=%d\n", getpid());// se obtiene el id del proceso
 		signal(SIGUSR1, senales);//envia la senal
 		sleep(5);//se hace un sleep de 5 segundos en pantalla
